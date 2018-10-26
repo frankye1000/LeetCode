fe08f4517d3aa980fa1978f1da4e0979fb1d2948
Frank Ye(葉詠富) <frank_ye@asus.com>
2018-10-26 17:26:18 +0800

    push shortest distance to a character

---
+++ Shortest Distance to a Character.py
@@ -0,0 +1,9 @@
+S = "loveleetcode"
+C = 'e'
+# Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+
+Cindex = [i for i, v in enumerate(S) if v == C]
+print([min([abs(i - j) for j in Cindex]) for i in range(len(S))])
+
+
+
