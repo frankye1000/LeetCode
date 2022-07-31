4704756798bce06c1a91777c60055b71db0a1e36
frankye1000 <frankye100@gmail.com>
2022-07-31 15:59:33 +0800

    Valid parentheses

---
+++ c++/Valid_Parentheses.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    bool ans=true;
+    stack<char> st;
+
+    if(s[0]==')' || s[0]==']' || s[0]=='}'){
+        ans=false;
+
+    }else{
+        for(int i=0; i<s.length(); i++){
+            if(!st.empty() && st.top()=='(' && s[i]==')'){
+                st.pop();
+            }else if(!st.empty() && st.top()=='[' && s[i]==']'){
+                st.pop();
+            }else if(!st.empty() && st.top()=='{' && s[i]=='}'){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        
+    }
+    
+
+
+    
+    if(!st.empty()) ans=false;
+    
+    cout<<ans<<endl;
+    
+    return 0;
+}
