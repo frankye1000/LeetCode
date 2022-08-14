5b390842aaea5726477cb2f729c8ce769e5e0355
frankye1000 <frankye100@gmail.com>
2022-08-14 16:56:39 +0800

    finish 209

---
+++ c++/209_Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int target=4;
+    vector<int> nums={1,1,1};
+    int i=0;
+    int sum=0;
+    int ans=INT32_MAX;
+    for(int j=0;j<nums.size();j++){
+        sum += nums[j];
+        while(sum>=target){
+            
+            int len = j-i+1;
+            cout<<"j= "<<j<<"i= "<<i<<"sum= "<<sum<<endl;
+            if(ans>len) ans = len;
+            sum-=nums[i];
+            i++;
+        }
+    }
+    if(ans==INT32_MAX) cout<<0<<endl;
+    else cout<<ans<<endl;
+
+
+    return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
